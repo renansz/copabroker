ddf554af953f2b75ba647af6d56015f7f9f29ff8
Renan Zanelatto <renansz@gmail.com>
2013-10-18 01:31:37 -0300

    ajustada a tela do broker com o js que gera a visualização em grupos e ranking com os dados do banco

--- broker/static/broker.js
+++ broker/static/broker.js
@@ -1,7 +1,24 @@
 $(document).ready(function() {
-    
-    $("#content-2").hide();
+	/* sort the teams based on the registered prices -- descending order*/
+	var teams = [];
+	$("tr[name=team]").each(function(){
+		teams.push([$(this).find("td[name=price]").html().replace(",","."),$(this).find("td[name=country_name] a").html()])
+	});
+	teams.sort(function(a,b) {return Number(b[0]) - Number(a[0]);});
+	/* end of sort */
+	
+	$("#content-2").hide();
 
+	/*make the ranking visualization*/
+	$.each(teams,function(idx,val){
+		$("tbody[name=content-2]").append('<tr><td>'+idx+'</td><td name="country_name-2">'+val[1]+'</td><td name="price-2" class="text-right">'+val[0].replace(".",",")+'</td></tr>');
+    });
+    	
+    	
+    	
+    	
+    
+    
     $("#btn-group").click(function(){
 	$("#content-2").hide();
 	$("#content").show();
