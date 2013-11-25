c16f0c194af3d5893ff3e4ea96ecdfed3b04dda0
Renan Zanelatto <renansz@gmail.com>
2013-11-25 02:04:05 -0200

    bandeirinhas renderizadas na 'home' com logica de nomes de bandeiras a partir de ticker e sufixo sm , md e lg

--- broker/static/broker.js
+++ broker/static/broker.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   /* sort the teams based on the registered prices -- descending order*/
   var teams = [];
   $("tr[name=team]").each(function(){
-    teams.push([$(this).find("td[name=price]").html().replace(",","."),$(this).find("td[name=name] a").html()]);
+    teams.push([$(this).find("td[name=price]").html().replace(",","."),$(this).find("td[name=name] a").html(),$(this).find("td[name=flag]").html()]);
   });
   teams.sort(function(a,b) {return Number(b[0]) - Number(a[0]);});
   /* end of sort */
@@ -11,7 +11,7 @@ $(document).ready(function() {
   
   /*make the ranking visualization*/
   $.each(teams,function(idx,val){
-    $("tbody[name=content-2]").append('<tr><td>'+Number(idx+1)+'</td><td name="name-2"><a href="book">'+val[1]+'</a></td><td name="price-2" class="text-right">'+val[0].replace(".",",")+'</td></tr>');
+    $("tbody[name=content-2]").append('<tr><td>'+Number(idx+1)+'</td><td>'+val[2]+'</td><td name="name-2"><a href="book">'+val[1]+'</a></td><td name="price-2" class="text-right">'+val[0].replace(".",",")+'</td></tr>');
   });
     
   $("#btn-group").click(function(){
